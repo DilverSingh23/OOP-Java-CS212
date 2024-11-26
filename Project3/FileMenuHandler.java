9e18e55cf7594473bedfc76c5b2b69abb1b0d5ed
DilverSingh23 <dsinghh23@gmail.com>
2024-11-25 22:05:21 -0500

    Fix variable names in fmh

--- Project3/FileMenuHandler.java
+++ Project3/FileMenuHandler.java
@@ -105,27 +105,24 @@ public class FileMenuHandler implements ActionListener {
         // We iterate through nodes using .next until our node reaches a null node meaning we've reached the end of the linked list
         // The text shown on the GUI is the toString created for each node in Refrigerator.java 
         // The sorted linked lists are populated with data of type appliance
-        JTextArea fridgeArea = new JTextArea();
         ApplianceNode currentRefrigerator = refrigerators.first.next;
         while (currentRefrigerator != null){
-            fridgeArea.append(currentRefrigerator.data.toString() + "\n");
+            refrigeratorArea.append(currentRefrigerator.data.toString() + "\n");
             currentRefrigerator = currentRefrigerator.next;
         }
-        frame.add(new JScrollPane(fridgeArea));
+        frame.add(new JScrollPane(refrigeratorArea));
 
         // Dishwasher section will be the second column 
         // The logic for this section will follow the refirgerator
-        JTextArea dishWasherArea = new JTextArea();
         ApplianceNode currentDishwasher = dishwashers.first.next; 
         while (currentDishwasher != null){
-            dishWasherArea.append(currentDishwasher.data.toString() + "\n");
+            dishwasherArea.append(currentDishwasher.data.toString() + "\n");
             currentDishwasher = currentDishwasher.next; 
         }
-        frame.add(new JScrollPane(dishWasherArea));
+        frame.add(new JScrollPane(dishwasherArea));
 
         // Microwave section will be the last column 
         // Logic for this section follows the refrigerator and microwave code
-        JTextArea microwaveArea = new JTextArea();
         ApplianceNode currentMicrowave = microwaves.first.next;
         while (currentMicrowave != null){
             microwaveArea.append(currentMicrowave.data.toString() + "\n");
